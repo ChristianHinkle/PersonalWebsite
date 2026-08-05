a4557e4030d5ec4c263a90ee518891dc07171d1e
Christian Hinkle <christianhinko@gmail.com>
2026-08-04 19:07:21 -0600

    [social_media] Convert the rest of social media links to be moved to the `SocialMedia` static class, in one place.

--- ChristianHinko/ChristianHinko/Classes/SocialMedia.cs
+++ ChristianHinko/ChristianHinko/Classes/SocialMedia.cs
@@ -12,5 +12,41 @@
 
         public const string EmailAddress = "christianthinkle@gmail.com";
         public const string EmailAddressHyperlink = $"mailto:{EmailAddress}";
+
+        public const string TwitterProfileUsername = "ChristianHinko";
+        public const string TwitterProfileUrl = $"https://twitter.com/{TwitterProfileUsername}";
+
+        public const string InstagramProfileUsername = "christianhinko";
+        public const string InstagramProfileUrl = $"https://instagram.com/{InstagramProfileUsername}";
+
+        public const string FacebookProfileUsername = "ChristianHinko";
+        public const string FacebookProfileUrl = $"https://facebook.com/{FacebookProfileUsername}";
+
+        public const string YouTubeProfileUsername = "ChristianHinko";
+        public const string YouTubeProfileUrl = $"https://youtube.com/@{YouTubeProfileUsername}";
+
+        public const string TwitchProfileUsername = "christianhinko";
+        public const string TwitchProfileUrl = $"https://twitch.tv/{TwitchProfileUsername}";
+
+        public const string TikTokProfileUsername = "christianhinko";
+        public const string TikTokProfileUrl = $"https://tiktok.com/@{TikTokProfileUsername}";
+
+        public const string ArtStationProfileUsername = "christianhinko";
+        public const string ArtStationProfileUrl = $"https://artstation.com/{ArtStationProfileUsername}";
+
+        public const string DeviantArtProfileUsername = "christianhinko";
+        public const string DeviantArtProfileUrl = $"https://deviantart.com/{DeviantArtProfileUsername}";
+
+        public const string PixivProfileUsername = "christianhinko";
+        public const string PixivProfileUrl = $"https://pixiv.net/users/88014865";
+
+        public const string RedditProfileUsername = "ChristianHinko";
+        public const string RedditProfileUrl = $"https://reddit.com/user/{RedditProfileUsername}";
+
+        public const string PinterestProfileUsername = "ChristianHinko";
+        public const string PinterestProfileUrl = $"https://pinterest.com/{PinterestProfileUsername}";
+
+        public const string TumblrProfileUsername = "christianhinko";
+        public const string TumblrProfileUrl = $"https://tumblr.com/blog/{TumblrProfileUsername}";
     }
 }
